cb7e18c19c62309401ccfe421b30f8ca4595c857
Samuell0003 <2003samuellucas@gmail.com>
2023-09-21 15:13:07 -0300

    add test client

--- src/Client.java
+++ src/Client.java
@@ -20,6 +20,28 @@ public class Client {
                 '4', '0', '1', '2', '3', '1', '0', '2', '1', '8', '4', '5', // bit 62, número do cartão 4012-3102-1845
                 '1' // bit 62, forma de pagamento 1 - débito, 2 - crédito
         };
+
+         byte[] m_msgBytes_0200_saldo_indisponível = {
+                '0', '2', '0', '0', //tipo da mensagem - 200 é uma mensagem de requisição de transação financeira
+                '0', '0', '0', '0', '0', '0', '0', '1', '2', '1', '1', '5', // bit 4, valor em centavos - neste
+                '1', '0', '4', '4', '4', '6', // bit 12, hora local da transacao - 10:44:46
+                '0', '5', '1', '2', // bit 13, data da transacao 05/12
+                '0', '4', '0', '1', '0', '4', // bit 33, rede transmissora
+                '4', '0', '1', '2', '3', '1', '0', '2', '1', '8', '4', '5', // bit 62, número do cartão 4012-3102-1845
+                '1' // bit 62, forma de pagamento 1 - débito, 2 - crédito
+        };
+
+         byte[] m_msgBytes_0200_cartão_inexistente = {
+                '0', '2', '0', '0', //tipo da mensagem - 200 é uma mensagem de requisição de transação financeira
+                '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '5', // bit 4, valor em centavos - neste
+                '1', '0', '4', '4', '4', '6', // bit 12, hora local da transacao - 10:44:46
+                '0', '5', '1', '2', // bit 13, data da transacao 05/12
+                '0', '4', '0', '1', '0', '4', // bit 33, rede transmissora
+                '0', '0', '1', '2', '3', '1', '4', '2', '1', '7', '4', '5', // bit 62, número do cartão 4012-3102-1845
+                '1' // bit 62, forma de pagamento 1 - débito, 2 - crédito
+        };
+
+
         saida.write(m_msgBytes_0200);
         byte[] data = new byte[46];
         entrada.readFully(data);
